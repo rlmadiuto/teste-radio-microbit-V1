385cf4dd3dbd8bc22260bdcff8306663a119b46c
rlmadiuto <96079616+rlmadiuto@users.noreply.github.com>
2022-02-26 17:14:13 -0300

    Atualizar 'main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -172,10 +172,38 @@ input.onGesture(Gesture.Shake, function () {
     radio.sendValue("limpa", 1)
     shakeshake()
 })
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("C")
+    radio.sendNumber(0)
+})
 input.onButtonPressed(Button.B, function () {
     basic.showString("B")
     btb += 1
     radio.sendValue("btB", btb)
+})
+radio.onReceivedValue(function (name, value) {
+    if (name == "btA") {
+        basic.showIcon(IconNames.Heart)
+        basic.pause(10)
+        basic.showNumber(value)
+    }
+    if (name == "btB") {
+        basic.showIcon(IconNames.Heart)
+        basic.pause(10)
+        basic.showNumber(value)
+    }
+    if (name == "fim") {
+        explode()
+        if (value == 1) {
+            radio.sendValue("fim", 0)
+        }
+    }
+    if (name == "limpa") {
+        basic.clearScreen()
+        bta = 0
+        btb = 0
+        shakeshake()
+    }
 })
 let btb = 0
 let bta = 0
